921b4f69d4e6a008aba40ab2bb11e7c5170c9125
trong.tn178@gmail.com <trong.tn178@gmail.com>
2020-04-12 16:18:15 +0700

    [login] Init layout

---
+++ app/src/main/java/hcmute/spkt/tranngoctrong/food_delivery/SignInActivity.java
@@ -0,0 +1,14 @@
+package hcmute.spkt.tranngoctrong.food_delivery;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SignInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_sign_in);
+    }
+}
---
+++ app/src/main/java/hcmute/spkt/tranngoctrong/food_delivery/model/Food.java
@@ -0,0 +1,5 @@
+package hcmute.spkt.tranngoctrong.food_delivery.model;
+
+public class Food {
+
+}
