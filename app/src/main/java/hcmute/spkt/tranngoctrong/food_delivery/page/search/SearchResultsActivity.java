9ccea5710ebf8b9051ce3f1740044567a28af1cd
Trong Tran <trong.tn178@gmail.com>
2020-05-20 21:34:36 +0700

    [search-results] Init fragment

---
+++ app/src/main/java/hcmute/spkt/tranngoctrong/food_delivery/page/search/SearchResultsActivity.java
@@ -0,0 +1,13 @@
+package hcmute.spkt.tranngoctrong.food_delivery.page.search;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+public class SearchResultsActivity extends AppCompatActivity {
+
+//    private SectionsPagerAdapter sectionsPagerAdapter;
+
+    private ViewPager viewPage;
+
+
+}
---
+++ app/src/main/java/hcmute/spkt/tranngoctrong/food_delivery/page/search/search_results_fragment/CommonResults.java
@@ -0,0 +1,3 @@
+package hcmute.spkt.tranngoctrong.food_delivery.page.search.search_results_fragment;
+
+public class CommonResults {}
